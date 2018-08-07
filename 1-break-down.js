1d622fe50e1fa558d1ad651c6118d5366f064cb6
João Viana <joaopadraoviana@gmail.com>
2018-08-07 10:54:11 +0200

    initial

---
+++ 1-break-down.js
@@ -0,0 +1,17 @@
+let input = 'lumo'
+let expected = 'dee'
+let output;
+
+console.log("String(arg).charAt(2) + String('ee');\n");
+console.log("- " + input + " should become " + expected);
+
+
+ { 
+    const step_1 = input.charAt(2); 
+    // str: 'm'
+    const step_2 = step_1 + 'ee'
+    // str: 'mee'
+    output = step_2;
+};
+
+console.log("- actual: " + output );
